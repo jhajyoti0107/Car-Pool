b99b29fbe5dbfea71e36a8d365616d0be1c4b33e
jhajyoti0107 <jhajyoti166@gmail.com>
2026-02-19 11:27:33 +0530

    Delete DataStore.java

--- DataStore.java
+++
@@ -1,7 +0,0 @@
-import java.util.ArrayList;
-
-public class DataStore {
-    public static ArrayList<User> users = new ArrayList<>();
-    public static ArrayList<Ride> rides = new ArrayList<>();
-    public static ArrayList<Booking> bookings = new ArrayList<>();
-}
