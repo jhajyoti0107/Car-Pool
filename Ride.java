c340c7a9e7917877b935f864bf9ef24758360cbb
jhajyoti0107 <jhajyoti166@gmail.com>
2026-02-19 11:33:29 +0530

    Update Ride.java

--- Ride.java
+++ Ride.java
@@ -1,27 +1,15 @@
 public class Ride {
-    int id;
+
+    int rideId;
     String source;
     String destination;
-    int total_seats;
-    int available_seats;
-    double fare;
-    User user; // driver
+    int seats;
+    double price;
 
-    public Ride(int id, String source, String destination, int total_seats, double fare, User user) {
-        this.id = id;
+    public Ride(String source, String destination, int seats, double price) {
         this.source = source;
         this.destination = destination;
-        this.total_seats = total_seats;
-        this.available_seats = total_seats;
-        this.fare = fare;
-        this.user = user;
-    }
-
-    public boolean bookSeats(int seats) {
-        if (seats <= available_seats) {
-            available_seats -= seats;
-            return true;
-        }
-        return false;
+        this.seats = seats;
+        this.price = price;
     }
 }
